53a9ae37f2e3dca72ce9092d07ea491af2c48e0c
mash <51327918+Mashkiki@users.noreply.github.com>
2024-11-12 20:45:57 +0100

    fuck canvas, changed the whole rendering code
    replaced canvas rendering with normal html elements lol

---
+++ testing/js/player.js
@@ -0,0 +1,16 @@
+let player = {
+  gold: 0,
+  rebirth_stones: 0,
+  inventory: {
+    pets: {},
+    shiny_skins: {},
+    metallic_skins: {
+      chrome: {},
+      gold: {},
+      diamond: {},
+      obsidian: {},
+      titanium: {},
+    },
+  },
+  titanium_ingots: 0,
+};
